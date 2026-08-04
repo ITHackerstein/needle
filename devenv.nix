542b643f2470a46a9d5f9e3374068ea1db29680b
Davide Carella <carelladavide1@gmail.com>
2026-08-04 18:11:19 +0200

    Initial commit

---
+++ devenv.nix
@@ -0,0 +1,12 @@
+{ pkgs, lib, config, inputs, ... }:
+{
+  languages.python = {
+    enable = true;
+    package = pkgs.python313;
+
+    uv = {
+      enable = true;
+      sync.enable = false;
+    };
+  };
+}
